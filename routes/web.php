a3b94d0152b89c30b010d66a8a42d8567dc6447d
FelipeSalesInovcorp <felipe.sales@trainee.inovcorp.com>
2026-02-04 18:44:58 +0000

    feat(entidades): add Criar/editar fluxo com Inertia, Ziggy e permissões

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,14 @@ Route::get('dashboard', function () {
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/entities', [EntityController::class, 'index'])->name('entities.index');
+
+    // 👇 NOVAS ROTAS
+    Route::get('/entities/create', [EntityController::class, 'create'])->name('entities.create');
+    Route::get('/entities/{entity}/edit', [EntityController::class, 'edit'])->name('entities.edit');
+
     Route::post('/entities', [EntityController::class, 'store'])->name('entities.store');
     Route::put('/entities/{entity}', [EntityController::class, 'update'])->name('entities.update');
     Route::delete('/entities/{entity}', [EntityController::class, 'destroy'])->name('entities.destroy');
 });
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
